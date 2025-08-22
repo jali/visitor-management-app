52eeb9fa7cf9a1421a92f5ab556226ccc98921ee
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2025-08-22 23:40:31 +0100

    change domain

--- backend/src/constants/index.js
+++ backend/src/constants/index.js
@@ -1,4 +1,4 @@
 // backend/src/constants.js
 module.exports = {
-  CLIENT_BASE_URL: process.env.NODE_ENV === 'production' ? 'https://halaween.com' : 'http://192.168.1.188:3000',
+  CLIENT_BASE_URL: process.env.NODE_ENV === 'production' ? 'https://abqa.net' : 'http://192.168.1.188:3000',
 };
